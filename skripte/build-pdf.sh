ccb74393b434d9609c6fb7d618b14638512c18b4
joergs <joergs@fe66431f-f319-0410-8186-f4e9b7e7c232>
2005-12-19 12:21:28 +0000

    * build-pdf.sh so umgebaut, dass jetzt die einzelnen Skripte nach jedem   commit gebaut werden können
    git-svn-id: https://svn.lug-jena.de/uni-skripte@248 fe66431f-f319-0410-8186-f4e9b7e7c232

--- skripte/build-pdf.sh
+++ skripte/build-pdf.sh
@@ -2,10 +2,28 @@
 
 set -e -u
 
-URL=file:///home/stud/md01/joergs/.svnroot/
-WEB=/home/stud/md01/joergs/.www/skripte/pdf/
+if [ $# -eq 0 ]; then
+    echo "usage: $0 Repository" >&2
+    exit 1
+fi
 
-exec >$WEB/build.log 2>&1
+REPOS=$1
+URL=/home/stud/md01/joergs/.svnroot/$REPOS
+
+if ! [ -d "$URL" ]; then
+    echo "$REPOS is not a valid repository in /home/stud/md01/joergs/.svnroot/" >&2
+    exit 1
+fi
+URL=file://$URL
+WEB=/home/stud/md01/joergs/.www/skripte/
+
+if [ -e "$WEB/pdf/$REPOS.tar.gz" ] && ( stamp="$(stat -c %y $WEB/pdf/$REPOS.tar.gz)";
+     [ "${stamp%% *}" \> "$(svnlook date ${URL#*://} |cut -d\  -f1)" ]); then
+    echo "I: $REPOS is up to date"
+    exit 0
+fi
+
+# exec >$WEB/pdf/$REPOS.build.log 2>&1
 
 export LANG=C
 
@@ -16,53 +34,37 @@ TMP=$(mktemp -d)
 
 cd $TMP
 
-for i in carl-ana1 carl-ana2 engelbert-mass-integral erhard-komm-sys \
-  fey-recharch \
-  fichtner-ewms hecker-parallel linde-stochastik lischke-form-sprachen \
-  schmeisser-ana3 schmeisser-hoehere-ana vogel-dml2 vogel-info4; do
+svn export $URL
 
-    echo "I: Repository $i"
+GZIP=-9 tar -czf $WEB/pdf/$REPOS.tar.gz $REPOS
+echo "I: created $REPOS.tar.gz"
 
-    if [ -e "$WEB/$i.tar.gz" ] && ( stamp="$(stat -c %y $WEB/$i.tar.gz)";
-         [ "${stamp%% *}" \> "$(svnlook date ${URL#*://}/$i |cut -d\  -f1)" ]); then
-        echo "I: $i is up to date"
-        continue
-    fi
-
-    svn export $URL/$i
-
-    GZIP=-9 tar -czf $WEB/$i.tar.gz $i
-    echo "I: created $i.tar.gz"
-
-    if [ -r $i/skript.latex ]; then
-        src=skript.latex
-    elif [ -r $i/skript.tex ]; then
-        src=skript.tex
-    elif [ "$(ls $i/*.latex $i/*.tex 2>/dev/null | wc -l)" -eq 1 ]; then
-        src=$(ls $i/*.latex $i/*.tex 2>/dev/null) || true
-        src=${src#$i/}
-    else
-        echo "E: No latex file found in $i"
-        rm -r $TMP/*
-        continue
-    fi
-    echo "I: using $src as source file"
-
-    cp $WEB/../sty/* $i
-    for ext in pdf ps; do
-        if rubber --$ext $i/$src && [ -e ${src%.*}.$ext ]; then
-            echo "I: installing ${src%.*}.$ext as $i.$ext"
-            rm -f $WEB/$i.$ext
-            cp ${src%.*}.$ext $WEB/$i.$ext
-        else
-            echo "E: building ${src%.*}.$ext failed"
-        fi
-    done
-
+if [ -r $REPOS/skript.latex ]; then
+    src=skript.latex
+elif [ -r $REPOS/skript.tex ]; then
+    src=skript.tex
+elif [ "$(ls $REPOS/*.latex $REPOS/*.tex 2>/dev/null | wc -l)" -eq 1 ]; then
+    src=$(ls $REPOS/*.latex $REPOS/*.tex 2>/dev/null) || true
+    src=${src#$REPOS/}
+else
+    echo "E: No latex file found in $REPOS"
     rm -r $TMP/*
+    exit 1
+fi
+echo "I: using $src as source file"
 
-    echo "I: ...done."
+cp $WEB/sty/* $REPOS
+for ext in pdf ps; do
+    if rubber --$ext $REPOS/$src && [ -e ${src%.*}.$ext ]; then
+        echo "I: installing ${src%.*}.$ext as $REPOS.$ext"
+        rm -f $WEB/pdf/$REPOS.$ext
+        cp ${src%.*}.$ext $WEB/pdf/$REPOS.$ext
+    else
+        echo "E: building ${src%.*}.$ext failed"
+    fi
 done
 
 cd /
-rmdir $TMP
+rm -r $TMP
+
+echo "I: ...done."
